f552f8ac52768e969756f2eb74b512965bbaeb4e
itang <live.tang@gmail.com>
2013-07-24 20:55:32 +0800

    add s2 js version

---
+++ s2/s2.js
@@ -0,0 +1,40 @@
+var sys = require('sys');
+
+function ofuscatedPassword(pass) {
+    var ret = "";
+	  var plen = pass.length;
+	  if (plen < 3) {
+		    ret = _repeatStar(plen);
+	  } else {
+		    ret = pass[0] + _repeatStar(plen-2) + pass[plen-1];
+	  }
+
+	  debug(ret);
+	  return ret;
+}
+
+function _repeatStar(times) {
+	  return _repeatStr("*", times)
+}
+
+function _repeatStr(str, times) {
+    var arr = [];
+	  for(var i= 0; i < times; i++) {
+        arr[i] = str
+	  }
+	  return arr.join("");
+}
+
+function debug(o) {
+	  sys.debug(o);
+}
+
+function assert(t) {
+    if(!t) throw "AssertionError"
+}
+
+assert("" === ofuscatedPassword(""));
+assert("*" === ofuscatedPassword("s"));
+assert("**" === ofuscatedPassword("ss"));
+assert("s*s" === ofuscatedPassword("sss"));
+assert("s****s" === ofuscatedPassword("ssaass"));
